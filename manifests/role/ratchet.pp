3de2abc53a0796937f229fc5297933b65c42690f
Karl Pietri <barnaclebob@gmail.com>
2016-01-20 17:03:18 -0600

    set root mumble node name

--- manifests/role/ratchet.pp
+++ manifests/role/ratchet.pp
@@ -1,3 +1,12 @@
-class wrenchies::role::master {
+class wrenchies::role::ratchet {
+  # set mumble root name
 
+  ini_setting { 'mumble root name':
+    ensure  => present,
+    path    => '/etc/mumble-server.ini',
+    setting => 'registerName',
+    value   => 'LegalTender',
+    notify  => Service['mumble-server'],
+    require => Package['mumble-server'],
+  }
 }
