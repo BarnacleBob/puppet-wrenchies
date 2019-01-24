cf644525a0b755100bce06bd45f6c0b993eff12d
Karl Pietri <barnaclebob@gmail.com>
2019-01-24 13:29:46 -0800

    get rid of smf redirect root to www

--- manifests/role/legaltender/smf.pp
+++ manifests/role/legaltender/smf.pp
@@ -1,25 +1,25 @@
 class wrenchies::role::legaltender::smf {
-  class { '::smforum':
-    user           => 'www-data',
-    document_root  => '/opt/smforum',
-    manage_mysql   => true,
-    mysql_user     => 'smforum',
-    mysql_db       => 'smforum',
-    mysql_password => 'legaltendersmforum',
-    include_php    => true,
-    manage_vhost   => true,
-    vhost_ssl      => true,
-    vhost_ssl_only => true,
-    vhost_fqdn     => 'legaltender.net',
-    vhost_type     => 'nginx',
-    vhost_ssl_key  => '/data/ssl/nginx/lt.key',
-    vhost_ssl_cert => '/data/ssl/nginx/lt.cert',
-    require        => [
-      File['/opt/smforum'],
-      File['/data/ssl/nginx/lt.key'],
-      File['/data/ssl/nginx/lt.cert']
-    ],
-  }
+#  class { '::smforum':
+#    user           => 'www-data',
+#    document_root  => '/opt/smforum',
+#    manage_mysql   => true,
+#    mysql_user     => 'smforum',
+#    mysql_db       => 'smforum',
+#    mysql_password => 'legaltendersmforum',
+#    include_php    => true,
+#    manage_vhost   => true,
+#    vhost_ssl      => true,
+#    vhost_ssl_only => true,
+#    vhost_fqdn     => 'legaltender.net',
+#    vhost_type     => 'nginx',
+#    vhost_ssl_key  => '/data/ssl/nginx/lt.key',
+#    vhost_ssl_cert => '/data/ssl/nginx/lt.cert',
+#    require        => [
+#      File['/opt/smforum'],
+#      File['/data/ssl/nginx/lt.key'],
+#      File['/data/ssl/nginx/lt.cert']
+#    ],
+#  }
 
 
   include '::nginx'
@@ -37,6 +37,12 @@ class wrenchies::role::legaltender::smf {
     location_cfg_append => { 'rewrite' => '^ https://legaltender.net$request_uri? permanent' },
   }
 
+  nginx::resource::vhost { 'legaltender.net':
+    ensure              => present,
+    www_root            => '/data/vhost/legaltender.net/',
+    location_cfg_append => { 'rewrite' => '^ https://www.legaltender.net$request_uri? permanent' },
+  }
+  
   file { '/data/ssl/nginx':
     ensure => 'directory',
     owner  => 'www-data',
